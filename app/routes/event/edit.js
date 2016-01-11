13e1b0a7cb87caa8702b7ba43d009ccc3b79ad0a
Tomasz Subik <tsubik@gmail.com>
2016-01-11 18:32:15 +0100

    rollback event changes if not saved

--- app/routes/event/edit.js
+++ app/routes/event/edit.js
@@ -7,6 +7,18 @@ export default Ember.Route.extend({
             const newUser = this.store.createRecord("user");
 
             event.get("users").pushObject(newUser);
+        },
+
+        saveChanges() {
+            const event = this.currentModel;
+
+            event.save().then(() => this.transitionTo("event.overview"));
+        },
+
+        willTransition() {
+            const event = this.currentModel;
+
+            event.rollbackAttributes();
         }
     }
 });
