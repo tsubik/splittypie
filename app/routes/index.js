ff2afa6fe70c90b2098073962ceaaffb1bae76e1
Tomasz Subik <tsubik@gmail.com>
2016-02-03 15:29:07 +0100

    previous events on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+    localStorage: Ember.inject.service(),
+
+    model() {
+        return {
+            previousEvents: this.get("localStorage").findAll("events"),
+        };
+    },
 });
