84c338cfc00f6457550a77406079181b1bbb1755
Tomasz Subik <tsubik@gmail.com>
2016-02-19 14:37:29 +0100

    balance list and settlement list style changes

--- app/components/user-balance-list-item.js
+++ app/components/user-balance-list-item.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 export default Ember.Component.extend({
     tagName: "tr",
     classNames: ["user-balance-list-item"],
-    classNameBindings: ["isPositive:success:danger"],
+    // classNameBindings: ["isPositive:success:danger"],
 
     isPositive: Ember.computed.gte("user.balance", 0),
 });
