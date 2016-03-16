7c5e29506c2a4571e965ce12651bd1cd27c8494f
Tomasz Subik <tsubik@gmail.com>
2016-03-16 13:38:53 +0100

    fixing back button

--- app/components/back-button.js
+++ app/components/back-button.js
@@ -2,9 +2,11 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
     tagName: "button",
-    classNames: ["btn", "btn-link", "navbar-btn"],
+    classNames: ["btn", "btn-link"],
 
     click() {
         window.history.back();
+
+        return false;
     },
 });
