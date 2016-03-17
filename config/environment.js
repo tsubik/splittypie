534229b349b8e4a2bd65d34e60085c2ea8a49672
Tomasz Subik <tsubik@gmail.com>
2016-03-17 18:53:22 +0100

    changing locationType to hash

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,7 @@ module.exports = function (environment) {
             },
         },
         baseURL: "/",
-        locationType: "auto",
+        locationType: "hash",
         EmberENV: {
             FEATURES: {
                 // Here you can enable experimental features on an ember canary build
