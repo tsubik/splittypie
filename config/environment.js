0650bfba7b79a8a7e3838618aa61eec3f787fca7
Tomasz Subik <tsubik@gmail.com>
2016-03-21 14:03:10 +0100

    deploy to surge.sh

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,7 @@ module.exports = function (environment) {
             },
         },
         baseURL: "/",
-        locationType: "hash",
+        locationType: "auto",
         EmberENV: {
             FEATURES: {
                 // Here you can enable experimental features on an ember canary build
