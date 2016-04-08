2668f2acbf3821780eeefb180eafe69092c35b32
Tomasz Subik <tsubik@gmail.com>
2016-04-08 18:14:47 +0200

    Your name, your friend's name

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -3,6 +3,11 @@ import Ember from "ember";
 export default Ember.Component.extend({
     tagName: "li",
     classNames: ["user-form"],
+    placeholder: Ember.computed("index", function () {
+        const index = this.get("index");
+
+        return index === 0 ? "Your name" : "Your friend's name";
+    }),
 
     actions: {
         delete(user) {
