8a8cb10b9456842e5502df684e4416a5f37dc477
Tomasz Subik <tsubik@gmail.com>
2016-06-03 18:49:12 +0200

    side menu is a separate ember addon now

--- app/components/side-menu-link-to.js
+++
@@ -1,9 +0,0 @@
-import Ember from "ember";
-
-export default Ember.LinkComponent.extend({
-    sideMenu: Ember.inject.service(),
-
-    click() {
-        this.get("sideMenu").hide();
-    },
-});
---
+++ app/components/side-menu-toggle-custom.js
@@ -0,0 +1,6 @@
+import SideMenuToggle from "ember-side-menu/components/side-menu-toggle";
+
+export default SideMenuToggle.extend({
+    tagName: "button",
+    classNames: ["navbar-btn", "btn", "btn-link", "pull-left"],
+});
